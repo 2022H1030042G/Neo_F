7fa0a0be3deb211b3afdd01ddf4c138f9d758d57
AdwitiyaMishra <adwt.mishra@gmail.com>
2021-02-16 13:29:33 +0530

    Delete Neo_Finance2 directory

--- Neo_Finance2/src/main/java/com/example/Neo_Finance/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Neo_Finance.repository;
-
-import com.example.Neo_Finance.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
